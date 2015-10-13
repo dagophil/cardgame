8681a8314429a35c2b71d9d8ed41955a91f921da
Philip Schill <philip.schill@gmx.de>
2015-10-14 00:25:00 +0200

    Added comment to login model.

--- core/login_model.py
+++ core/login_model.py
@@ -3,16 +3,13 @@ import events
 
 class LoginModel(object):
     """
-    The client's model of the game.
+    The model for the login view. Since all information can be stored in the input widgets, this model does nothing.
     """
 
     def __init__(self, ev_manager):
         assert isinstance(ev_manager, events.EventManager)
         self._ev_manager = ev_manager
         self._ev_manager.register_listener(self)
-
-        self._host = ""
-        self._port = ""
 
     def notify(self, event):
         """
